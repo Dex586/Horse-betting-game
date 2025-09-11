91deba9af5f7ecb0b8d359e3e72ceeb1313d4d48
Samm <samm8gg@gmail.com>
2025-09-11 22:13:21 +0300

    Create constants.py to keep settings in a connected file

---
+++ constants.py
@@ -0,0 +1,4 @@
+TRACK_PIXELS = 1500 # Whole runaway field length
+LEFT_MARGIN = 60 # Space between horses and their names
+STEP_MIN, STEP_MAX = 1, 3 # Step to move, random
+BALANCE_FILE = "balance.txt" # File to keep balance, a.k.a your money
